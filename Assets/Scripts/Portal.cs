d7e2615b4ddf0d99563f3bdfa37c2d5389c57387
Andrew Ragan <andrew.s.ragan@vanderbilt.edu>
2017-04-20 11:27:36 +0200

    Portals

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit.");
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("HIT");
+        }
+    }
+
+}
