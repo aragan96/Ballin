79048a5e549d3033479ab06c81fcb4c5e7c015fe
prentzepis <prentzepis@gmail.com>
2017-04-20 10:35:00 +0200

    Enemy Script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    Vector3 playerPos;
+    Vector3 direction;
+    GameObject player;
+    Rigidbody rigbod;
+    public float speed;
+
+    void Start()
+    {
+        rigbod = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        var direction = Vector3.zero;
+        Debug.Log(player.transform.position);
+        direction = player.transform.position - transform.position;
+        direction.Normalize();
+        rigbod.velocity = direction * speed;
+    }
+}
