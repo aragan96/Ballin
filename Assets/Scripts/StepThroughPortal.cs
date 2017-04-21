413f70d34f4942e3820da5056860e21d5f7b79ee
prentzepis <prentzepis@gmail.com>
2017-04-21 17:12:51 +0200

    Portals!

---
+++ Assets/Scripts/StepThroughPortal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepThroughPortal : MonoBehaviour {
+
+    public GameObject otherPortal;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.position = otherPortal.transform.position + otherPortal.transform.forward*1;
+
+            // change the angle of the camera when you go through a portal
+
+            /*
+            Quaternion CameraRotation = Quaternion.LookRotation(otherPortal.transform.forward);
+            GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
+            Vector3 offset = CameraRotation * new Vector3(0, 0, 1);
+            camera.transform.position = other.transform.position + offset;
+            camera.transform.rotation = Quaternion.LookRotation(other.transform.position - camera.transform.position, new Vector3(0, 1, 0));*/
+        }
+    }
+}
---
+++ Assets/Scripts/ThrowPortal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowPortal : MonoBehaviour {
+
+    public GameObject leftPortal;
+    public GameObject rightPortal;
+
+    LayerMask playerMask;
+
+    GameObject mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Alpha3)){
+            throwPortal(leftPortal);
+        }
+        if (Input.GetKey(KeyCode.Alpha4))
+        {
+            throwPortal(rightPortal);
+        }
+	}
+
+    void throwPortal(GameObject portal){
+        int x = Screen.width / 2;
+        int y = Screen.height / 2;
+
+        Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y));
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            Quaternion hitObjectRotation = Quaternion.LookRotation(hit.normal);
+            portal.transform.position = hit.point;
+            portal.transform.rotation = hitObjectRotation;
+        }
+    }
+}
