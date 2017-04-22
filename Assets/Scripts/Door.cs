4404ba21439a3831ec2f2b2884596df7bd9b22ba
Andrew Ragan <andrew.s.ragan@vanderbilt.edu>
2017-04-22 20:08:50 +0200

    Stage Portals

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+
+    public bool locked;
+    public string levelName;
+    public GameObject portal;
+
+    void Start()
+    {
+        locked = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(!locked && other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(levelName);
+        }   
+    }
+}
