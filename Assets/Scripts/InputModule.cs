dc2bfe85b7dd67c99ec4b363475a998fc8af78ae
prentzepis <prentzepis@gmail.com>
2017-04-24 11:26:15 +0200

    cam baby steps

--- Assets/Scripts/InputModule.cs
+++ Assets/Scripts/InputModule.cs
@@ -2,31 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputModule : MonoBehaviour {
+public class InputModule : MonoBehaviour
+{
 
     PlayerController playerController;
     CameraController camController;
 
+    RightAngleCam rotatingCameraController;
+
     public Camera cam;
 
-	void Start () {
+    void Start()
+    {
         playerController = GetComponent<PlayerController>();
         camController = cam.GetComponent<CameraController>();
-	}
-	
-	void Update()
+        rotatingCameraController = cam.GetComponent<RightAngleCam>();
+    }
+
+    void Update()
     {
         float camHorizontal = Input.GetAxis("Mouse Y");
         float camVertical = Input.GetAxis("Mouse X");
         camController.camInput = new Vector2(camHorizontal, camVertical);
 
+        rotatingCameraController.camRotate = Input.GetKey("space");
+
         playerController.growInput = Input.GetKey(KeyCode.Alpha1);
         playerController.shrinkInput = Input.GetKey(KeyCode.Alpha2);
     }
 
-	void FixedUpdate () {
+    void FixedUpdate()
+    {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         playerController.movementInput = new Vector2(moveHorizontal, moveVertical);
     }
 }
+
