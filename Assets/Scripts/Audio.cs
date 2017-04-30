b38e21f3855ffc12d77019c9c17d5cddbfc03881
monicanimm <monicanimm@gmail.com>
2017-04-30 20:28:53 +0200

    sounds

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio:MonoBehaviour{
+
+	void Awake() {
+		GameObject [] objs = GameObject.FindGameObjectsWithTag ("music");
+		if (objs.Length > 1)
+			Destroy (this.gameObject);
+		DontDestroyOnLoad (this.gameObject);
+}
+}
