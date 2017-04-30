b0384844e065a3600767eda2b3f50cc620a43511
monicanimm <monicanimm@gmail.com>
2017-04-30 15:20:11 +0200

    asfjajhfkj

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Fire : MonoBehaviour {
-	GameObject player;
+	//GameObject player;
 	//Rigidbody playerrb;
 
 	// Use this for initialization
 	void Start () {
 		//playerrb = GameObject.FindGameObjectWithTag ("Player").GetChild(2).GetComponent<Rigidbody>();
 
-		player=GameObject.FindGameObjectWithTag("Player");
+		//player=GameObject.FindGameObjectWithTag("Player");
 	}
 	void Update () {
 
@@ -18,8 +18,10 @@ public class Fire : MonoBehaviour {
 
 
 	void OnParticleCollision(GameObject player){
+		Debug.Log ("hit fire");
 			Rigidbody body = player.GetComponent<Rigidbody>();
 			if (body) {
+			
 				player.GetComponentInParent<PlayerController> ().GoToCheckpoint ();
 				Destroy (player);
 			}
