90bb81312b827f838131651e1dd7b0e6c15d281b
prentzepis <prentzepis@gmail.com>
2017-04-30 19:07:13 +0200

    All levels done?

--- Assets/Scripts/DeathTile.cs
+++ Assets/Scripts/DeathTile.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class DeathTile : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
 	void OnTriggerEnter(Collider other){
 		if (other.tag == "Player") {
             Physics.gravity = new Vector3(0,-Mathf.Abs(Physics.gravity.y),0);
